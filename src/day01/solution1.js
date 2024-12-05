d9c1a4392cc8ed9953102f60e78f9227c0013ada
Carl-Johan Erikson <103150338+CurlBall@users.noreply.github.com>
2024-12-05 17:57:26 +0100

    Refactor into reusable functions

--- src/day01/solution1.js
+++ src/day01/solution1.js
@@ -8,26 +8,35 @@
 const fs = require('node:fs')
 const inputPath = './input.txt'
 
+main()
+
 function main() {
-    const file = fs.readFileSync(inputPath, {encoding: 'utf-8'})
-    const strPairs = file.split('\n')
-    const left = []
-    const right = []
-    strPairs.forEach(pair => {
-        const numPair = pair.split('   ') 
-        left.push(numPair[0])
-        right.push(numPair[1])
-    })
+    const [left, right] = readFilePairs(inputPath, '   ')
 
     left.sort()
     right.sort()
 
     let absDiff = 0
-    for(let i = 0; i < strPairs.length; i++) {
+    for(let i = 0; i < left.length; i++) {
         const diff = left[i] - right[i]
         absDiff += Math.abs(diff)
     }
 
     console.log(absDiff)
 }
-main()
+
+function readFilePairs(filePath, separator) {
+    const file = fs.readFileSync(filePath, {encoding: 'utf-8'})
+    const strPairs = file.split('\n')
+
+    const left = []
+    const right = []
+    strPairs.forEach(pair => {
+        const numPair = pair.split(separator) 
+        left.push(numPair[0])
+        right.push(numPair[1])
+    })
+    return [left, right]
+}
+
+module.exports = { readFilePairs, inputPath }
